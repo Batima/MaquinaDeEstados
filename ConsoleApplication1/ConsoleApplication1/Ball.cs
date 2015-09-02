3816531dbb945be9c84cf14bb0980c9393dd4afa
Wendell Machado <wendellmachado.nave@gmail.com>
2015-09-02 14:13:55 -0300

    Done

---
+++ ConsoleApplication1/ConsoleApplication1/Ball.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public enum BallState
+    {
+        Stoped, Rolling, Airbone, Curling
+    }
+
+    class Ball
+    {
+        protected BallState state;
+
+        public void Pass()
+        {
+            state = BallState.Rolling;
+        }
+
+        public void FreeKick()
+        {
+            state = BallState.Curling;
+        }
+
+        public void LongPass()
+        {
+            state = BallState.Airbone;
+        }
+
+        public void Dominate()
+        {
+            state = BallState.Stoped;
+        }
+
+        public void GetState()
+        {
+            Console.WriteLine("\nBall State: " + state.ToString() + "\n");
+        }
+
+        public void Play(int value)
+        {
+            switch (value)
+            {
+                case 1: Pass(); break;
+                case 2: LongPass(); break;
+                case 3: FreeKick(); break;
+                case 4: Dominate(); break;
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ball ball = new Ball();
+            Console.WriteLine("Rola a bola na tela da Globo");
+            ball.GetState();
+            String read = null;
+            while (read != "q" && (read == null || read == ""))
+            {
+                int action = Actions();
+                ball.Play(action);
+                ball.GetState();
+                read = Console.ReadLine();
+            }
+        }
+
+        static int Actions()
+        {
+            String read = null;
+            int value = -1;
+            while (value <= 0 || value > 4)
+            {
+                Console.WriteLine("Choose an action");
+                Console.WriteLine("1 - Pass");
+                Console.WriteLine("2 - Long pass");
+                Console.WriteLine("3 - Freekick");
+                Console.WriteLine("4 - Dominate");
+                Console.WriteLine("");
+                read = Console.ReadLine();
+                Int32.TryParse(read, out value);
+            }
+            return value;
+        }
+    }
+}
